6bba473b4e1dc6f78dcd83a96cfe1b04c0e1eef9
hoffbaaj <hoffbaaj@mail.uc.edu>
2022-09-22 13:08:31 -0400

    1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        double num = 150;
+        double tax = 0.02;
+        double cost = (num + (num * tax));
+
+        if(num >= 100)
+        {
+            System.out.println("The total price is " + num);
+        }
+        else
+        {
+            System.out.println("The total price is " + cost);
+        }
     }
 }
